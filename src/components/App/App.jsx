ee7b011196f6810b4af832b951554fdf71ed2b98
IgorSokolyanskyy <1988isokol@gmail.com>
2023-01-26 22:48:52 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,19 +6,39 @@ import Notification from 'components/Notification';
 import css from '../App/App.module.css';
 
 export default function App() {
-  const [state, setState] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const { good, neutral, bad } = state;
+  // const [state, setState] = useState({
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0,
+  // });
 
-  const addFeedback = options =>
-    setState(state => ({ ...state, [options]: state[options] + 1 }));
+  // const { good, neutral, bad } = state;
+
+  const feedback = { good, neutral, bad };
+
+  const addFeedback = options => {
+    // setState(state => ({ ...state, [options]: state[options] + 1 }));
+    switch (options) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+      default:
+        return;
+    }
+  };
 
   const countTotalFeedback = () =>
-    Object.values(state).reduce((value, acc) => acc + value, 0);
+    Object.values(feedback).reduce((value, acc) => acc + value, 0);
 
   const countPositiveFeedbackPercentage = () =>
     Math.round((good / countTotalFeedback()) * 100) || 0;
@@ -27,7 +47,8 @@ export default function App() {
     <div className={css.card}>
       <Section title="Please leave feedback">
         <FeedbackOptions
-          options={Object.keys(state)}
+          // options={Object.keys(state)}
+          options={Object.keys(feedback)}
           onLeaveFeedback={addFeedback}
         />
       </Section>
